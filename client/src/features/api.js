da47e60de1a7775aaf9ed1fb7ce80808a0c8a954
CarlinChow <88292021+CarlinChow@users.noreply.github.com>
2022-03-22 18:29:41 -0700

    fixed posting order bug

--- client/src/features/api.js
+++ client/src/features/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "/api",
     prepareHeaders: (headers, { getState }) => {
       if(getState().auth.user !== null){
         const { token } = getState().auth.user
